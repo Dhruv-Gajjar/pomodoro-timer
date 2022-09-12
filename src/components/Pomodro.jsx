4a620c044eace1fbc462de614e1e798bd8cf9ca6
Dhruv-Gajjar <dhruvgajjar07@gmail.com>
2022-09-12 13:09:21 +0530

    Add pause, reset and switch mode

--- src/components/Pomodro.jsx
+++ src/components/Pomodro.jsx
@@ -5,29 +5,57 @@ import { GrPowerReset } from 'react-icons/gr'
 import { useEffect, useState } from "react";
 
 function Pomodro({ user }) {
-    const [minutes, setMinutes] = useState(25);
+    const [workTime, setWorkTime] = useState(25);
+    const [breakTime, setBreakTime] = useState(5);
     const [seconds, setSeconds] = useState(0);
     const [timer, setTimer] = useState();
     const [timerMode, setTimerMode] = useState(false);
+    const [mode, setMode] = useState('work');
 
-    const timeMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const workMinutes = workTime < 10 ? `0${workTime}` : workTime;
+    const breakMinutes = breakTime < 10 ? `0${breakTime}` : breakTime;
     const timeSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
     const start = () => {
-        let timer = setInterval(() => {
-            if (seconds === 0) {
-                setMinutes(minutes - 1);
-                setSeconds(59);
-            } else {
-                setSeconds(seconds - 1)
+        let interval = setInterval(() => {
+            if (mode === 'work') {
+                if (workTime === 0 && seconds === 0) {
+                    clearInterval(timer);
+                } else if (seconds === 0) {
+                    setWorkTime(workTime - 1)
+                    setSeconds(59);
+                } else {
+                    setSeconds(seconds - 1)
+                }
             }
 
-            setTimer(timer);
-        }, 1000);
+            if (mode === 'break') {
+                if (breakTime === 0 && seconds === 0) {
+                    clearInterval(timer);
+                } else if (seconds === 0) {
+                    setBreakTime(breakTime - 1)
+                    setSeconds(59);
+                } else {
+                    setSeconds(seconds - 1)
+                }
+            }
+
+            setTimer(interval);
+        }, 10);
     }
 
     const signout = async () => {
         await signOut(auth);
+    }
+
+
+    const reset = () => {
+        setTimer();
+        clearInterval(timer);
+        setTimerMode(false);
+        setWorkTime(25);
+        setBreakTime(5);
+        setSeconds(0);
     }
 
     useEffect(() => {
@@ -36,12 +64,19 @@ function Pomodro({ user }) {
             start();
         }
 
-        if (minutes === 0 && seconds === 0) {
-            clearInterval(timer);
+        if (workTime === 0 && seconds === 0) {
+            setMode('break');
+            setBreakTime(5);
+        }
+
+        if (breakTime === 0 && seconds === 0) {
+            setMode('work')
+            setWorkTime(25);
         }
 
         return () => { clearInterval(timer); }
-    }, [timer, seconds, timerMode, minutes])
+    }, [timer, seconds, timerMode, mode])
+
 
     return (
         <>
@@ -52,13 +87,15 @@ function Pomodro({ user }) {
 
             <div className="flex flex-col items-center justify-center space-y-8">
                 <h1 className="mt-10">Pomodro Timer</h1>
-
-                <div className="text-6xl">{timeMinutes} : {timeSeconds}</div>
+                {mode === 'work' ? "WORK" : "BREAK"}
+                <div className="text-6xl">{
+                    mode === 'work' ? <div>{workMinutes} : {timeSeconds}</div> : <div>{breakMinutes} : {timeSeconds}</div>
+                }</div>
 
                 <div className="flex items-center justify-between space-x-6 mt-8 cursor-pointer">
                     <AiOutlinePlayCircle size={50} onClick={() => setTimerMode(true)} />
                     <AiOutlinePauseCircle size={50} onClick={() => setTimerMode(false)} />
-                    <GrPowerReset size={50} />
+                    <GrPowerReset size={50} onClick={reset} />
                 </div>
             </div>
         </>
